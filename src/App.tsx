5dfe3e6e37e8938fba466ab848ef5436469c2f00
AboIbrahemSy <abo.ibrahem.org@gmail.com>
2024-03-14 01:54:53 +0300

    edit app file for get account & create a Contract

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,76 @@
-
+import { useState,useEffect } from 'react'
+import abi from "./artifacts/contracts/chai.sol/chai.json"
+import {ethers} from "ethers"
 import Buy from './components/Buy'
 import './App.css'
 
+interface StateType{
+    provider:any;
+    signer:any;
+    contract:any;
+}
+
+declare global {
+  interface Window {
+      // ethereum: import('ethers').providers.ExternalProvider;
+      ethereum: any
+  }
+}
+
 function App() {
+  const [state,setState]=useState<StateType>({
+    provider:null,
+    signer:null,
+    contract:null
+  })
+  const [account,setAccount]=useState<string>('Not connected');
+
+  useEffect(()=>{
+    const template=async()=>{
+   
+      const contractAddres="0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9";
+      const contractABI=abi.abi;
+      //Metamask part
+      //1. In order do transactions on goerli testnet
+      //2. Metmask consists of infura api which actually help in connectig to the blockhain
+      try{
+
+        const {ethereum}=window;
+        const account = await ethereum.request({
+          method:"eth_requestAccounts"
+        })
+ 
+        window.ethereum.on("accountsChanged",()=>{
+         window.location.reload()
+        })
+        setAccount(account);
+        const provider = new ethers.providers.Web3Provider(ethereum);//read the Blockchain
+        const signer =  provider.getSigner(); //write the blockchain
+        
+        const contract = new ethers.Contract(
+          contractAddres,
+          contractABI,
+          signer
+        )
+        console.log(contract)
+      setState({provider,signer,contract});
+       
+      }catch(error){
+        console.log(error)
+      }
+    }
+    template();
+  },[])
+
+
   return (
     <div >
     <p style={{ marginTop: "10px", marginLeft: "5px" }}>
-      <small>Connected Account - account</small>
+      <small>Connected Account - {account}</small>
     </p>
    
       <Buy />
-      
+      {/* <Buy state={state} /> */}
    
   </div>
   )
